a63eb180d7aa1b47d34c9837d8580b9c9f366bf0
silvermuru <silx99@gmail.com>
2017-08-16 20:19:24 +0300

    Resellers map fix

--- page-contacts.php
+++ page-contacts.php
@@ -47,23 +47,25 @@ get_header(); ?>
         <div class="col-md-12 header-grey-box">
             <h1>Resellers</h1>
         </div>
-        <div class="acf-map">
-        <?php
-        if( have_rows('resellers') ):
-            while ( have_rows('resellers') ) : the_row();
-            ?>
-            <div class="col-md-12">
-                <?php
-                $location = get_sub_field('map');
-                if( !empty($location) ):
-                ?>
-                    <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
-                <?php endif; ?>
-            </div>
+        <div class="col-md-12">
+            <div class="acf-map">
             <?php
-            endwhile;
-        endif;
-        ?>
+            if( have_rows('resellers') ):
+                while ( have_rows('resellers') ) : the_row();
+                ?>
+                <div class="col-md-12">
+                    <?php
+                    $location = get_sub_field('map');
+                    if( !empty($location) ):
+                    ?>
+                        <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+                    <?php endif; ?>
+                </div>
+                <?php
+                endwhile;
+            endif;
+            ?>
+            </div>
         </div>
     </div>
 </div>
