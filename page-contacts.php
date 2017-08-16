4339c9075f43df54dcda83042968218d2e7338ef
silvermuru <silx99@gmail.com>
2017-08-16 20:23:01 +0300

    Info window for reseller map added

--- page-contacts.php
+++ page-contacts.php
@@ -58,7 +58,11 @@ get_header(); ?>
                     $location = get_sub_field('map');
                     if( !empty($location) ):
                     ?>
-                        <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+                        <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>">
+                            <p class="address">
+                                <?php get_sub_field('mailing_aadress'); ?>
+                            </p>
+                        </div>
                     <?php endif; ?>
                 </div>
                 <?php
